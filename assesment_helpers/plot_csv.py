bb801450fa795dd3d7a7422998e7b6a5b14c3f32
Dave Carroll <davezcarroll@gmail.com>
2021-10-13 16:00:50 -0700

    created acleaning and plotting scripts in assesment_helpers directory

---
+++ assesment_helpers/plot_csv.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+#This script uses seaborn to plot "clean_ga_data.csv" (aka the output from to_csv.py)
+
+sns.set_theme(color_codes=True)
+
+plotData = pd.read_csv("clean_GA_Data.csv")
+
+#ax = sns.regplot(x="Generation", y="Best Fitness", data=plotData)
+#ax = sns.regplot(x="Generation", y="Average Fitness", data=plotData)
+ax = sns.regplot(x="Generation", y="Average Score", data=plotData)
+plt.show()
---
+++ assesment_helpers/to_csv.py
@@ -0,0 +1,41 @@
+import csv
+
+#This script formats the "GAdata.txt" file (created by trainGeneticAlgorithm.py) as a .csv file named "clean_ga_data.csv"
+#Note: make sure "GAdata.txt" is named exactly that and is in the same directory as this script
+#Note: if "clean_ga_data.csv" already exists in this directory it will be overwritten
+
+ga_data = open("GAdata.txt", "r")
+
+clean_ga_data = open("clean_ga_data.csv", "w")
+
+csv_writer = csv.writer(clean_ga_data)
+csv_writer.writerow(["Generation", "Best Fitness", "Average Fitness", "Average Score"])
+
+line = ga_data.readline()
+
+while(line):
+    if("Generation" in line):
+        row = []
+        
+        split = line.split(" ")
+        row.append(split[1].strip())
+        
+        line = ga_data.readline() # skip binary line
+        
+        line = ga_data.readline()
+        split = line.split(" ")
+        row.append(split[2].strip()) 
+        
+        line = ga_data.readline()
+        split = line.split(" ")
+        row.append(split[2].strip())
+        
+        line = ga_data.readline()
+        split = line.split(" ")
+        row.append(split[3].strip())
+        
+        csv_writer.writerow(row)
+    line = ga_data.readline() #iterate
+
+ga_data.close()
+clean_ga_data.close()
