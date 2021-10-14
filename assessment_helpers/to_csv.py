b0efce7fd9d0466f10a192dd129c07781fd9d4db
Dave Carroll <davezcarroll@gmail.com>
2021-10-13 21:54:12 -0700

    added command line argument parsing for to_csv.py

--- assessment_helpers/to_csv.py
+++ assessment_helpers/to_csv.py
@@ -3,39 +3,52 @@
 #Note: if "ga_data.csv" already exists in this directory it will be overwritten
 
 import csv
+import argparse
+import os
 
-ga_data = open("GAdata.txt", "r")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ga_data_filename", nargs='?', type=str, default="GAdata.txt")
+    parser.add_argument("--output_filename", nargs='?', type=str, default="ga_data.csv")
+    args = parser.parse_args()
 
-csv_ga_data = open("ga_data.csv", "w")
+    ga_data = open(args.ga_data_filename, "r")
 
-csv_writer = csv.writer(csv_ga_data)
-csv_writer.writerow(["Generation", "Best Fitness", "Average Fitness", "Average Score"])
+    output_ext = os.path.splitext(args.output_filename)[1]
 
-line = ga_data.readline()
+    if(output_ext != ".csv"):
+        raise Exception("output file must be a .csv")
 
-while(line):
-    if("Generation" in line):
-        row = []
-        
-        split = line.split(" ")
-        row.append(split[1].strip())
-        
-        line = ga_data.readline() # skip binary line
-        
-        line = ga_data.readline()
-        split = line.split(" ")
-        row.append(split[2].strip()) 
-        
-        line = ga_data.readline()
-        split = line.split(" ")
-        row.append(split[2].strip())
-        
-        line = ga_data.readline()
-        split = line.split(" ")
-        row.append(split[3].strip())
-        
-        csv_writer.writerow(row)
-    line = ga_data.readline() #iterate
+    csv_ga_data = open(args.output_filename, "w")
 
-ga_data.close()
-csv_ga_data.close()
+    csv_writer = csv.writer(csv_ga_data)
+    csv_writer.writerow(["Generation", "Best Fitness", "Average Fitness", "Average Score"])
+
+    line = ga_data.readline()
+
+    while(line):
+        if("Generation" in line):
+            row = []
+            
+            split = line.split(" ")
+            row.append(split[1].strip())
+            
+            line = ga_data.readline() # skip binary line
+            
+            line = ga_data.readline()
+            split = line.split(" ")
+            row.append(split[2].strip()) 
+            
+            line = ga_data.readline()
+            split = line.split(" ")
+            row.append(split[2].strip())
+            
+            line = ga_data.readline()
+            split = line.split(" ")
+            row.append(split[3].strip())
+            
+            csv_writer.writerow(row)
+        line = ga_data.readline() #iterate
+
+    ga_data.close()
+    csv_ga_data.close()
