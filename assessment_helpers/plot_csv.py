f6f8996c208d57b641b31f861e3aa6c24abed963
Dave Carroll <davezcarroll@gmail.com>
2021-10-13 21:22:24 -0700

    adds command line argument parsing for plot_csv.py

--- assessment_helpers/plot_csv.py
+++ assessment_helpers/plot_csv.py
@@ -3,12 +3,21 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import argparse
 
-sns.set_theme(color_codes=True)
-
-plotData = pd.read_csv("clean_GA_Data.csv")
-
-#ax = sns.regplot(x="Generation", y="Best Fitness", data=plotData)
-#ax = sns.regplot(x="Generation", y="Average Fitness", data=plotData)
-ax = sns.regplot(x="Generation", y="Average Score", data=plotData)
-plt.show()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv_filename", nargs='?', type=str, default="../clean_ga_data.csv")
+    parser.add_argument("--figure_filename", nargs='?', type=str, default="")
+    args = parser.parse_args()
+    
+    sns.set_theme(color_codes=True)
+    
+    plotData = pd.read_csv(args.csv_filename)
+    #ax = sns.regplot(x="Generation", y="Best Fitness", data=plotData)
+    #ax = sns.regplot(x="Generation", y="Average Fitness", data=plotData)
+    ax = sns.regplot(x="Generation", y="Average Score", data=plotData)
+    plt.show()
+    
+    if (args.figure_filename):
+        plt.savefig(args.figure_filename)
